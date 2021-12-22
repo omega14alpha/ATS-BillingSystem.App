00330c2cebc29cab85659379930fca77cb174fae
omega14alpha <omega14alpha@gmail.com>
2021-12-22 10:28:57 +0300

    Added view model class and interface.

---
+++ ATS-BillingSystem.App/Infrastructure/ProgramViewModel.cs
@@ -0,0 +1,135 @@
+﻿using ATS_BillingSystem.App.ATS;
+using ATS_BillingSystem.App.BillingSystem;
+using ATS_BillingSystem.App.Models.Abonents;
+using ATS_BillingSystem.App.Models.Systems;
+using System;
+using System.Collections.Generic;
+
+namespace ATS_BillingSystem.App.Infrastructure
+{
+    internal class ProgramViewModel : IViewModel
+    {
+        private IStatisticsCollector _callStatistics;
+
+        private IList<IAbonent> _abonents;
+
+        private IAbonent _abonent;
+
+        private IAbonent _calledTestAbonent;
+
+        private Random _rand;
+
+        private PortController _portController;
+
+        private IStation _station;
+
+        private readonly string[] _firstnames = new[] { "Bob", "Jecob", "Fred", "Styu", "Sten", "Ann", "Tramp" };
+
+        private readonly string[] _surnames = new[] { "Jonson", "Calipso", "Adams", "Swift", "Simpson", "Yan", "Star", };
+
+        private readonly string _tarrifName = "Light";
+
+        public IEnumerable<IAbonent> AbonentsCollection => _abonents;
+
+        public IAbonent Abonent => _abonent;
+
+        public IAbonent CalledTestAbonent => _calledTestAbonent;
+
+        public ProgramViewModel()
+        {
+            _callStatistics = new StatisticsCollector();
+            _abonents = new List<IAbonent>();
+            _rand = new Random();
+
+            _portController = new PortController();
+            _station = new Station(_portController);
+
+            _station.OnRecordingCallStartData += _callStatistics.SaveNewCallStartData;
+            _station.OnRecordingCallEndData += _callStatistics.SaveNewCallEndData;
+        }
+
+        public void CreateTestAbonentsCollection(int testAbonentCount)
+        {
+            ITariffPlan tariffPlan = new TariffPlanLight()
+            {
+                PlanId = 1,
+                TarrifName = _tarrifName,
+                PriceOfOneMinute = 7
+            };
+
+            IClientManager clientManager = new ClientManager(_station, _portController);
+
+            for (int i = 0; i < testAbonentCount; i++)
+            {
+                string firstname = _firstnames[_rand.Next(0, _firstnames.Length)];
+                string surname = _surnames[_rand.Next(0, _surnames.Length)];
+                var abonent = clientManager.RegisterNewAbonent(firstname, surname, tariffPlan);
+                _callStatistics.AddNewAbonentToCollection(abonent.Contract.AbonentId);
+                _abonents.Add(abonent);
+            }
+        }
+
+        public void ChoiseRandomAbonent()
+        {
+            _abonent = _abonents[_rand.Next(0, _abonents.Count)];
+            _abonents.Remove(_abonent);
+        }
+
+        public void ChoiseRandomTargetTestAbonent()
+        {
+            _calledTestAbonent = _abonents[_rand.Next(0, _abonents.Count)];
+        }
+
+        public void ConnectToPort()
+        {
+            _abonent.ConnectToPort();
+        }
+
+        public void TestAbonentConnectToPort()
+        {
+            _calledTestAbonent.ConnectToPort();
+        }
+
+        public void DisconnectFromPort()
+        {
+            _abonent.DisconectFromPort();
+        }
+
+        public void CallToTestAbonent()
+        {
+            _abonent.InitiateStartCall(_calledTestAbonent.Contract.PhoneNumber);
+        }
+
+        public void StopCurrentCall()
+        {
+            _abonent.InitiateStopCall();
+        }
+
+        public void FillTestDataToStatisticHandler()
+        {
+            ICollection<IAbonentsHistory> testCollection = new List<IAbonentsHistory>();
+
+            for (int i = 1; i < 13; i++)
+            {
+                var dayCount = _rand.Next(30);
+                for (int f = 1; f < dayCount; f++)
+                {
+                    int randomValue = _rand.Next(86401);
+                    var beginDateTime = new DateTime(2021, i, f).AddSeconds(randomValue);
+                    var endDateTime = new DateTime(2021, i, f).AddSeconds(_rand.Next(randomValue, randomValue + 2000));
+                    var randomNumber = _abonents[_rand.Next(_abonents.Count)].Contract.PhoneNumber;
+                    var tempData = new AbonentsHistory() { BeginCallDateTime = beginDateTime, EndCallDateTime = endDateTime, TargetNumber = randomNumber };
+
+                    testCollection.Add(tempData);
+                }
+            }
+
+            _callStatistics.AddTestAbonentData(_abonent.Contract.AbonentId, testCollection);
+        }
+
+        public IEnumerable<IAbonentsHistory> GetCurrentAbonentStatistic()
+        {
+            return Abonent.GetStatistic(_callStatistics);
+        }
+    }
+}
---
+++ ATS-BillingSystem.App/Models/Abonents/TariffPlanLight.cs
@@ -0,0 +1,11 @@
+﻿namespace ATS_BillingSystem.App.Models.Abonents
+{
+    internal class TariffPlanLight : ITariffPlan
+    {
+        public int PlanId { get; set; }
+
+        public string TarrifName { get; set; }
+
+        public double PriceOfOneMinute { get; set; }
+    }
+}
